a54d41bc59d8969d111ca7927fd6f813dc98a111
Tammie <tammie@Tammies-MacBook-Pro.local>
2018-04-15 15:28:20 -0400

    enabled form submission

--- src/react/FindMyBusiness.jsx
+++ src/react/FindMyBusiness.jsx
@@ -1,18 +1,44 @@
-const React = require('React')
+import React from 'react';
 
 class FindMyBusiness extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      name: null,
-      zip: 0
-    }
+      name: '',
+      zip: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleName = this.handleName.bind(this);
+    this.handleZip = this.handleZip.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleName(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleZip(event) {
+    this.setState({zip: event.target.value});
   }
   
   render() {
     return (
-      <div className='alert alert-success'>
-        <p>Hello, World!</p>
+      <div className='entry'>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" name="name" value={this.state.name} className="name" onChange={this.handleName} />
+          </label>
+          <label>
+            Zipcode
+            <input type="text" name="zip" value={this.state.zip} className="zip" onChange={this.handleZip} />
+          </label>
+            <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
