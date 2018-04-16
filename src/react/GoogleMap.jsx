20b09948e254b06875dd02303a9d03115339072a
tammiet <tammie.thong@gmail.com>
2018-04-15 23:48:38 -0400

    map moves to searched position

--- src/react/GoogleMap.jsx
+++ src/react/GoogleMap.jsx
@@ -6,14 +6,6 @@ class GoogleMap extends React.Component {
   }
 
   componentDidMount() {
-    this.renderMap()
-  }
-
-  componentDidUpdate() {
-    this.renderMap()
-  }
-
-  renderMap() {
     this.map = new google.maps.Map(this.refs.map, {
       center: {
         lat: 48.858608,
@@ -22,26 +14,57 @@ class GoogleMap extends React.Component {
       zoom: 16
     });
 
+    this.marker = new google.maps.Marker({
+      map: this.map,
+      position: {
+        lat: 48.858608,
+        lng: 2.294471
+      }
+    });
+
+    this.geocoder = new google.maps.Geocoder();
+    this.renderMap()
+  }
+
+  componentDidUpdate() {
+    this.renderMap()
+  }
+
+  renderMap(map) {
     if (this.props.name) {
       let service = new google.maps.places.PlacesService(this.map)
         service.textSearch({
           query: this.props.name + ' ' + this.props.zip
         }, (results) => {
           console.log(results);
+          this.getResults(results)
         });
         service.getDetails({
           placeId: 'AIzaSyAzu1D8v-7FaPvVhKYqykGYpMc1WwZ70Mk'
         }, (results) => {
-          console.log(results);
+          // console.log(results);
         });
       }
+  }
+
+  getResults(results) {
+    let address = results[0].formatted_address;
+    this.geocoder.geocode({'address': address}, (results, status) => {
+      if (status === 'OK') {
+        this.map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: this.map,
+          position: results[0].geometry.location
+        });
+      }
+    })
   }
 
   render() {
     const {name, zip} = this.props;
 
     return (
-      <div ref="map" className='map' />
+        <div ref="map" className='map' />
     );
   }
 }
